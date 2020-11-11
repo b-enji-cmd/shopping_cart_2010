a06cfe747d6febba2039ddbd1b1c86bda5025a48
Ben Fulton <benjamin.fulton99@gmail.com>
2020-11-11 10:58:12 -0700

    Add ALMOST working total_inventory

--- lib/market.rb
+++ lib/market.rb
@@ -18,4 +18,14 @@ class Market
       vendor.inventory.include?(arg_item)
     end
   end
+
+  def total_inventory
+    breakdown = Hash.new{|h,k| h[k] = {quantity:0,vendors:[]}}
+    @vendors.each do |vendor|
+      vendor.inventory.each do |item,quantity|
+        breakdown[item][:quantity]+=quantity
+        breakdown[item][:vendors]<<vendors_that_sell(item)
+      end
+    end
+  end
 end
