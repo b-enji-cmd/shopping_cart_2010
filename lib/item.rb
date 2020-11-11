2c0d1a0ba0a51e40ef48b998afda7fb05b956d46
Ben Fulton <benjamin.fulton99@gmail.com>
2020-11-11 09:13:29 -0700

    Add floating point formatting for price

--- lib/item.rb
+++ lib/item.rb
@@ -1,7 +1,11 @@
 class Item
-  attr_reader :name, :price
+  attr_reader :name
   def initialize(info)
     @name = info[:name]
     @price = info[:price]
+  end
+
+  def price
+    @price[1..-1].to_f
   end
 end
