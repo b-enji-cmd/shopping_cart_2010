e213951626a8f3cdddb227b89b90f231be9e3946
Ben Fulton <benjamin.fulton99@gmail.com>
2020-11-11 09:39:23 -0700

    Add instance of test for market

---
+++ test/market_test.rb
@@ -0,0 +1,19 @@
+require './lib/item'
+require './lib/vendor'
+require './lib/market'
+require 'minitest/autorun'
+require 'pry'
+
+class MarketTest < Minitest::Test
+  def setup
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @market = Market.new("South Pearl Street Farmers Market")
+  end
+  def test_it_is
+    assert_instance_of Market, @market
+  end
+end
