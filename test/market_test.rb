2aa6b902a9a8ce1600804c9a43108d4ad14345df
Ben Fulton <benjamin.fulton99@gmail.com>
2020-11-11 09:52:30 -0700

    Add market add vendor test

--- test/market_test.rb
+++ test/market_test.rb
@@ -11,6 +11,8 @@ class MarketTest < Minitest::Test
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
     @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor3 = Vendor.new("Palisade Peach Shack")
     @market = Market.new("South Pearl Street Farmers Market")
   end
   def test_it_is
@@ -20,5 +22,21 @@ class MarketTest < Minitest::Test
   def test_it_has_attr
     assert_equal "South Pearl Street Farmers Market", @market.name
     assert_equal ([]), @market.vendors
+  end
+
+  def test_it_can_add_vendors
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+
+    @vendor3.stock(@item1, 65)
+
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    assert_equal [@vendor1, @vendor2, @vendor3] , @market.vendors
   end
 end
