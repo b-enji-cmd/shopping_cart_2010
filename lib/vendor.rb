bf2f695a7204af7fbd7d58279b68800ab6b83bfb
Ben Fulton <benjamin.fulton99@gmail.com>
2020-11-11 09:27:55 -0700

    Add passing check stock function

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -2,7 +2,10 @@ class Vendor
   attr_reader :name, :inventory
   def initialize(name)
     @name = name
-    @inventory = Hash.new{|h,k| h[k] = []}
+    @inventory = Hash.new{|h,k| h[k] = 0}
   end
 
+  def check_stock(arg_item)
+    @inventory[arg_item]
+  end
 end
